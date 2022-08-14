d2e26e2a4cfde6544e75b98962117fe664b99294
Tumbichi <germanviglietti@gmail.com>
2022-08-14 01:01:59 -0300

    feat: add update, delete get by id services and routes on categories

--- src/api/categories.routes.ts
+++ src/api/categories.routes.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import {
   createCategory,
+  deleteCategory,
   getAllCategories,
+  updateCategory,
 } from "../category/adapter/in/category.controller";
 
 const router = Router();
 
 router.post("/categories", createCategory);
 router.get("/categories", getAllCategories);
+router.delete("/categories/:id", deleteCategory);
+router.put("/categories", updateCategory);
 
 export default router;
--- src/category/adapter/in/category.controller.ts
+++ src/category/adapter/in/category.controller.ts
@@ -28,9 +28,53 @@ export const createCategory = async (req: Request, res: Response) => {
   }
 };
 
+export const updateCategory = async (req: Request, res: Response) => {
+  const { id, title } = req.body;
+
+  try {
+    const updatedProduct = await prisma.category.update({
+      data: {
+        title,
+      },
+      where: { id },
+    });
+
+    return res.status(200).json(updatedProduct);
+  } catch (e) {
+    console.log("updated category error", e);
+    return res.status(500).json(e);
+  }
+};
+
+export const deleteCategory = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const categoryDeleted = await prisma.category.delete({
+      where: { id: Number.parseInt(id) },
+    });
+    console.log("product delected", categoryDeleted);
+    return res.status(200).send(categoryDeleted);
+  } catch (e) {
+    console.log("product delete error", e);
+    return res.status(404).send("Category cannot deleted");
+  }
+};
+
 export const getAllCategories = async (req: Request, res: Response) => {
   try {
     const categories = await prisma.category.findMany();
+    return res.status(200).json(categories);
+  } catch (e) {
+    return res.status(500).json(e);
+  }
+};
+
+export const getCategoryById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const categories = await prisma.category.findUnique({ where: { id } });
     return res.status(200).json(categories);
   } catch (e) {
     return res.status(500).json(e);
