be1fe8a3c9b22dcaa53584f2832e180396eae45f
Tumbichi <germanviglietti@gmail.com>
2022-08-14 00:53:53 -0300

    feat: add routes to the products service: update and getById

--- src/api/products.routes.ts
+++ src/api/products.routes.ts
@@ -3,12 +3,16 @@ import {
   createProduct,
   deleteProduct,
   getAllProduct,
+  getProductById,
+  updateProduct,
 } from "../product/adapter/in/product.controller";
 
 const router = Router();
 
 router.post("/products", createProduct);
 router.delete("/products/:id", deleteProduct);
+router.put("/products/:id", updateProduct);
 router.get("/products", getAllProduct);
+router.get("/products/:id", getProductById);
 
 export default router;
